5b6bc0f6c69ff186bf1e53e7e7c1bdace6e83aba
Alexander Kosyakov <aekosyakov@gmail.com>
2019-09-30 20:00:19 +0300

    import daily utils

--- AVFStoreKitConfig.swift
+++ AVFStoreKitConfig.swift
@@ -1,4 +1,5 @@
 import Foundation
+import DailyUtils
 
 private
 let baseURL = "https://itunes.apple.com/lookup?id="
--- AVFStoreKitVC.swift
+++ AVFStoreKitVC.swift
@@ -1,4 +1,5 @@
 import UIKit
+import DailyUtils
 
 //        Example:
 //        let avfStoreKitConfig = AVFStoreKitConfig()
